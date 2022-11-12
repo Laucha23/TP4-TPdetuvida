4a4ae752fe76fb16ae9b3023785cbdc576400d7c
LautaroViola <80793147+Laucha23@users.noreply.github.com>
2022-11-12 00:38:26 -0300

    Delete comandos.sh

--- comandos.sh
+++
@@ -1,8 +0,0 @@
-# hacer correr al server
-g++ -pthread -g -o server server.cpp utils.cpp
-
-# hacer correr al client
-g++ -pthread -g -o client client.cpp utils.cpp
-
-# crear a los clients
-g++ -pthread -g -o clients creatorClients.cpp utils.cpp
